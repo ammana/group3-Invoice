20d8e88cde0f701d4b7615dc7528ebd99c13e384
ammana <mounikareddy.ammana@eagles.oc.edu>
2016-08-08 11:30:15 -0500

    Added ManageProject class and changes in HomePage and LoginPanel

---
+++ Invoice/src/gui/ManageProject.java
@@ -0,0 +1,333 @@
+package gui;
+
+import basicClasses.Project;
+import dataManagement.SystemData;
+import dataManagement.ConnectionManager;
+import java.util.Date;
+
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultComboBoxModel;
+
+public class ManageProject extends javax.swing.JPanel {
+    JFrame  panelHolder;
+    SystemData systemData;
+    DefaultComboBoxModel projectModel;
+    List<Project> projectList;
+    Project selectedProject;
+    
+    public ManageProject(JFrame  panelHolder, SystemData systemData) {
+        this.panelHolder = panelHolder;
+        this.systemData = systemData;  
+        initComponents();
+        
+        ConnectionManager cm = new ConnectionManager();
+        EntityManager em = cm.getEntityManager();
+        
+        Query query = em.createQuery("Select p  from Project p where p.managerName = '"
+                + systemData.getCurrentUser().getEmployee().getName()
+                + "'");
+        projectList = query.getResultList();
+        cm.close();
+        
+        projectModel = new DefaultComboBoxModel();
+        projectModel.addElement("Select Project");
+        for(Project project : projectList){  
+            projectModel.addElement(project.getName()+" ("+project.getId()+")");
+        }        
+        selectProjectComboBox.setModel(projectModel); 
+        
+        client.setText("");
+        clientContact.setText("");
+        budget.setText("");
+        startDate.setText("");
+        endDate.setText("");
+        status.setSelectedIndex(0);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jPanel2 = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        saveButton = new javax.swing.JButton();
+        cancelButton = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        endDate = new javax.swing.JTextField();
+        selectProjectComboBox = new javax.swing.JComboBox<>();
+        status = new javax.swing.JComboBox<>();
+        client = new javax.swing.JLabel();
+        startDate = new javax.swing.JLabel();
+        clientContact = new javax.swing.JLabel();
+        budget = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+
+        setLayout(new java.awt.GridBagLayout());
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jLabel2.setText("Select a Project");
+
+        jLabel3.setText("Client");
+
+        jLabel4.setText("Start Date");
+
+        jLabel5.setText("End Date");
+
+        saveButton.setText("Save");
+        saveButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveButtonActionPerformed(evt);
+            }
+        });
+
+        cancelButton.setText("Cancel");
+        cancelButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Status ");
+
+        jLabel8.setText("Client Contact ");
+
+        jLabel9.setText("Budget ");
+
+        selectProjectComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] {}));
+        selectProjectComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                selectProjectComboBoxActionPerformed(evt);
+            }
+        });
+
+        status.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "", "Closed" ,"In Progress"}));
+
+        client.setText("clientName");
+
+        startDate.setText("startDate");
+
+        clientContact.setText("clientContact");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(20, 20, 20)
+                                .addComponent(cancelButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(budget))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(clientContact, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(endDate)
+                            .addComponent(selectProjectComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(status, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(client, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(startDate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(selectProjectComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3)
+                    .addComponent(client, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(clientContact, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(budget, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(startDate))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(endDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(status, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cancelButton)
+                    .addComponent(saveButton))
+                .addContainerGap())
+        );
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Manage Project");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(94, 94, 94))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(4, 4, 4)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        add(jPanel2, new java.awt.GridBagConstraints());
+    }// </editor-fold>                        
+
+    private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        panelHolder.setTitle("Home Page");
+        panelHolder.getContentPane().removeAll();
+        panelHolder.getContentPane().add(new HomePage(panelHolder, systemData, false));
+        panelHolder.getContentPane().revalidate();
+
+    }                                            
+
+    private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        if(projectModel.getIndexOf(projectModel.getSelectedItem())==0){
+            JOptionPane.showMessageDialog(null, "Please select a Project first!");
+            return;
+        }        
+        try{
+            Integer.parseInt(budget.getText());
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Please enter integer value for Budget!");
+            return;
+        }
+        if(endDate.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Project End Date can not be blank!");
+            return;            
+        }
+        Date sDate;
+        Date eDate;
+        try{
+            sDate = new Date(startDate.getText());
+            eDate = new Date(endDate.getText());
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "End Date entered is invalid!");
+            return;
+        }
+        if(!eDate.after(sDate)){
+            JOptionPane.showMessageDialog(null, "Project End Date can not be before Start date!");
+            return;            
+        }
+        Date today = new Date();
+        if(eDate.compareTo(today)<0){
+            JOptionPane.showMessageDialog(null, "Project End Date should be after Today!");
+            return;            
+        }
+        
+        ConnectionManager cm = new ConnectionManager();
+        EntityManager em = cm.getEntityManager();
+
+        Project project = em.find(Project.class, selectedProject.getId());            
+        project.setBudget(Integer.parseInt(budget.getText()));  
+        project.setEndDate(endDate.getText());          
+        project.setStatus(status.getSelectedItem().toString());          
+
+        em.persist(project);
+        cm.close();
+
+        JOptionPane.showMessageDialog(null, "Change was saved suceessfully.");           
+
+        panelHolder.setTitle("Home Page");
+        panelHolder.getContentPane().removeAll();
+        panelHolder.getContentPane().add(new HomePage(panelHolder, systemData,false));
+        panelHolder.getContentPane().revalidate();
+    }                                          
+
+    private void selectProjectComboBoxActionPerformed(java.awt.event.ActionEvent evt) {                                                      
+        if(selectProjectComboBox.getSelectedIndex()==0){
+            selectedProject = null;
+            client.setText("");
+            clientContact.setText("");
+            budget.setText("");
+            startDate.setText("");
+            endDate.setText("");
+            status.setSelectedIndex(0);
+            return;
+        }
+        
+        Project project = projectList.get(selectProjectComboBox.getSelectedIndex()-1);   
+        selectedProject = project;
+        client.setText(project.getClient().getName());
+        clientContact.setText(project.getClientContact());
+        budget.setText(""+project.getBudget());
+        startDate.setText(project.getStartDate());
+        endDate.setText(project.getEndDate());
+        status.setSelectedItem(project.getStatus()); 
+    }                                                     
+
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JTextField budget;
+    private javax.swing.JButton cancelButton;
+    private javax.swing.JLabel client;
+    private javax.swing.JLabel clientContact;
+    private javax.swing.JTextField endDate;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton saveButton;
+    private javax.swing.JComboBox<String> selectProjectComboBox;
+    private javax.swing.JLabel startDate;
+    private javax.swing.JComboBox<String> status;
+    // End of variables declaration                   
+}
